d3e07ba8b864718a132ac437226160073140a937
José A. González <champunes@tutanota.com>
2023-12-01 16:11:59 +0100

    Implementar View Binding
    MainActivity conectada

--- app/src/main/java/com/iesaguadulce/pmdmtarea2/MainActivity.java
+++ app/src/main/java/com/iesaguadulce/pmdmtarea2/MainActivity.java
@@ -2,12 +2,16 @@ package com.iesaguadulce.pmdmtarea2;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.iesaguadulce.pmdmtarea2.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
