8bc52bed1509803698ce5ea8dfc47062f299b360
José A. González <champunes@tutanota.com>
2023-12-03 18:59:40 +0100

    Controlar foco
    Ocultar el teclado si hacemos foco fuera de los campos de texto

--- app/src/main/java/com/iesaguadulce/pmdmtarea2/MainActivity.java
+++ app/src/main/java/com/iesaguadulce/pmdmtarea2/MainActivity.java
@@ -1,14 +1,20 @@
 package com.iesaguadulce.pmdmtarea2;
 
+import android.content.Context;
+import android.graphics.Rect;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.textfield.TextInputEditText;
 import com.iesaguadulce.pmdmtarea2.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -40,5 +46,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item);
+    }
+
+    // Ocultar el teclado cuando se pulsa fuera de un campo de texto
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (v != null && v.getClass() == TextInputEditText.class) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int) ev.getRawX(), (int) ev.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+
+        return super.dispatchTouchEvent(ev);
     }
 }
